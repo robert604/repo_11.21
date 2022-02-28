36506ed8ddfc7e473f7f571379c911941f57f8b7
robert604 <robtemper@gmail.com>
2022-02-28 09:36:54 -0500

    for pull request

--- jsonserver.js
+++ jsonserver.js
@@ -13,5 +13,5 @@ server.use(router)
 const port = process.env.PORT || 3001
 
 server.listen(port, () => {
-  console.log(`JSON server is running on port ${port}`)
+  console.log(`JSON server is running on port: ${port}`)
 })
